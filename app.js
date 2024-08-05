4771384bf397dd77b82155a6f0142141a62555a6
thaobuihb <thaohb@gmail.com>
2024-08-06 05:44:25 +0700

    1

--- app.js
+++ app.js
@@ -1,18 +1,34 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+require("dotenv").config();
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+const cors = require("cors");
 
-var indexRouter = require('./routes/index');
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
 
 var app = express();
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-
+app.use(express.static(path.join(__dirname, "public")));
+app.use(
+  cors({
+    origin: "*",
+    credentials: true,
+  })
+);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use((req, res, next) => {
+  const error = new Error("No route found");
+  error.statusCode = 404;
+  next(error);
+});
+app.use((err, req, res, next) => {
+  res.status(err.statusCode).send(err.message);
+});
 module.exports = app;
