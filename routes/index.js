b8acc20cf2d8dfc943b23944e3315e0f5d8fe9fd
thaobuihb <thaohb@gmail.com>
2024-08-07 16:32:11 +0700

    update

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,12 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
+const pokemonsRouter = require("./pokemons.api");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", (req, res) => {
+  res.status(200).send("<h1>Welcome to Pokedex</h1>");
 });
 
+router.use("/pokemons", pokemonsRouter);
 
 module.exports = router;
